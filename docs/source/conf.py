cbe2ed0a1e944ec7c12d3b89bb40743671470bf6
Herminial <herminial@l909.info>
2024-08-19 16:05:53 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PuTTY App'
+copyright = '2024, Hermina'
+author = 'Hermina'
 
 release = '0.1'
 version = '0.1.0'
